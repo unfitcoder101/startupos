07f5417f5dbc316ea81c4e0fea36a058e9b14538
Harshvardhan <hvkasli007@gmail.com>
2026-06-18 00:36:17 +0530

    Add missing ProtectedRoute component

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from 'react-router-dom'
 
-function ProtectedRouted({ children }) {
+function ProtectedRoute({ children }) {
     const token = localStorage.getItem('token')
-
-    if(!token){
+    
+    if (!token) {
         return <Navigate to="/login" />
     }
+    
     return children
 }
 
